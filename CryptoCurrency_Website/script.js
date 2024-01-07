048be0f457f046dce8687a6b2454cad8772a41e4
JINESH <jineshkhandelwal12345@gmail.com>
2024-01-07 21:47:53 +0530

    First Commit

---
+++ CryptoCurrency_Website/script.js
@@ -0,0 +1,9 @@
+var setting={
+    "async":true,
+    "scrossDomain":true,
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=USD","method":"GET",
+    "headers":{}
+}
+$.ajax(settings).done(function(response){
+    console.log(response);
+})
